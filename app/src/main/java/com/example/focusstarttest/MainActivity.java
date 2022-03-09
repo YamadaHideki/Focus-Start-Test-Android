e3d3b6d30691c8fc55b474638c1aa84b6b6a2234
Soul <top.secret.work@yandex.ru>
2022-03-10 02:30:24 +0300

    Update code

--- app/src/main/java/com/example/focusstarttest/MainActivity.java
+++ app/src/main/java/com/example/focusstarttest/MainActivity.java
@@ -5,11 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -28,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
 
         try {
             String jsonResult = pool.submit(downloadJson).get();
+            CbrJson cbrJson = jsonToCbrObject(jsonResult);
+            Log.i("JSONResult", cbrJson.toString());
 
             try {
                 JSONObject jsonObject = new JSONObject(jsonResult);
@@ -35,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
                 Log.i("JSON", valute.toString());
                 /*for (int i = 0; i < valute.length(); i++) {
-                    Log.i("JSON", valute.getString("ID"));
+                    Log.i("JSON", valute.);
                 }*/
                 //Log.i("JSON Date", jsonObject.getString("Date"));
             } catch (JSONException e) {
@@ -48,5 +60,32 @@ public class MainActivity extends AppCompatActivity {
         }
 
         pool.shutdown();
+    }
+
+    public CbrJson jsonToCbrObject(String json) {
+        //List<T> result = new ArrayList<>();
+
+        var jsonElement = (JsonElement) JsonParser.parseString(json);
+        Log.i("JSONA", jsonElement.toString());
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+
+        var result = gson.fromJson(json, CbrJson.class);
+
+        try {
+            JSONObject jo = new JSONObject(json).getJSONObject("Valute");
+            Iterator<String> iterator = jo.keys();
+
+            while (iterator.hasNext()) {
+                String key = iterator.next();
+                Valute valute = gson.fromJson(jo.get(key).toString(), Valute.class);
+                valute.setTag(key);
+                result.addValute(valute);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
